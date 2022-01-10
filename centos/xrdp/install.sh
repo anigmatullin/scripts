d7034b08bcfb2e65de691594e335f682f10bd5d6
Aibulat <nigmatullin@gmail.com>
2022-01-10 20:49:53 +0500

    xrdp

---
+++ centos/xrdp/install.sh
@@ -0,0 +1,23 @@
+
+sudo dnf groupinstall "Server with GUI"
+
+sudo dnf install epel-release
+
+sudo dnf install xrdp
+
+sudo systemctl enable xrdp --now
+
+sudo systemctl status xrdp
+
+sudo firewall-cmd --new-zone=xrdp --permanent
+sudo firewall-cmd --zone=xrdp --add-port=3389/tcp --permanent
+sudo firewall-cmd --zone=xrdp --add-source=192.168.1.0/24 --permanent
+sudo firewall-cmd --reload
+
+echo "Make sure you don't have local sessions before testing RDP connection!"
+echo "Otherwise, the connection would drop immediatelly after login"
+echo "sudo tail -f /var/log/xrdp-sesman.log"
+echo "would show the following:"
+echo "Window manager (pid yyyyy, display xx) exited quickly (1 secs). This could indicate a window manager config problem"
+
+
