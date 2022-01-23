133bb5692a16cf26b09ef5309f0034e568711c2f
Aibulat <nigmatullin@gmail.com>
2022-01-23 14:50:45 +0500

    installer

---
+++ python/django/create_project.sh
@@ -0,0 +1,6 @@
+django-admin startproject prjname
+
+cd prjname
+
+python3 manage.py startapp appname
+
--- python/django/install.sh
+++ python/django/install.sh
@@ -4,9 +4,3 @@ pip3 install mssql-django
 pip3 install python-decouple
 
 python3 -m django --version
-
-django-admin startproject prjname
-
-cd notes
-
-python3 manage.py startapp notes
