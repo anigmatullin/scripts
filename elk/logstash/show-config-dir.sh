6ebedc7086070558ce736c9985acf1e756373461
Aibulat <nigmatullin@gmail.com>
2022-01-31 11:43:26 +0500

    logstash

---
+++ elk/logstash/show-config-dir.sh
@@ -0,0 +1,1 @@
+ls /etc/logstash/conf.d/
--- python/django/notes/meetups/models.py
+++ python/django/notes/meetups/models.py
@@ -2,8 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-from django.db import models
-
 
 class Author(models.Model):
     objects = models.Manager()
