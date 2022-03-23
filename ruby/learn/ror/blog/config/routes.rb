f3717f7fff3495a1cc85a8b255ca50feda6c91e9
Aibulat <nigmatullin@gmail.com>
2022-03-24 02:39:32 +0500

    scaffold test

--- ruby/learn/ror/blog/config/routes.rb
+++ ruby/learn/ror/blog/config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
+  resources :contacts
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
   root 'posts#index', as: 'home'
-
   get 'about' => 'pages#about', as: 'about'
-  get 'contacts' => 'pages#contacts', as: 'contacts'
-
-  resources :posts
 
 end
