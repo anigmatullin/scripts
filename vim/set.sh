498d91b2fae8d6cd6d685521eb998a3b644aedfb
Aibulat <nigmatullin@gmail.com>
2022-03-26 18:45:48 +0500

    neovim

--- vim/set.sh
+++ vim/set.sh
@@ -1,1 +1,7 @@
-cp -f vimrc ~/.vimrc
+cp -f vimrc  ~/.vimrc
+
+rm -fr ~/.config/nvim
+mkdir -p ~/.config/nvim/autoload
+cp -f plug.vim ~/.config/nvim/autoload
+
+cp -f vimrc  ~/.config/nvim/init.vim
