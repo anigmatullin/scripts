08c93ab95a89bd2d9f44467614d4c00af5efb036
Aibulat <nigmatullin@gmail.com>
2022-06-08 22:03:38 +0500

    start CRUD for Relay Groups

--- dev/node/express/src/routes/config.js
+++ dev/node/express/src/routes/config.js
@@ -7,6 +7,30 @@ const functions = require('../functions.js');
 
 module.exports = router;
 
-router.get("/relaygrp", (req, res) => {    
-    res.render("routing/index.ejs", {});
+router.get("/relaygrp", (req, res) => {
+
+    // let params = {
+    //     data: [
+    //         {
+    //             id: 1,
+    //             name: "Exch",
+    //             description: "Inbound"
+    //         },
+    //         {
+    //             id: 2,
+    //             name: "Outbound",
+    //             description: "Cloud Mail"
+    //         },
+    //     ]
+    // };
+
+    models.RelayGroup.findAll()
+    .then(data => {
+        let params = {
+            data: data
+        };
+        
+        res.render("routing/index.ejs", params);
+    })
+    
 });
